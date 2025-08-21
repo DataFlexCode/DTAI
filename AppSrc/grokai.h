1a2520fbaa0fd782a28028dd69d15f4f2b1a3087
matthewdavidian <matthewd@datatechag.com>
2025-08-20 19:46:46 -0700

    Added procedure to set timeout

---
+++ AppSrc/grokai.h
@@ -0,0 +1,13 @@
+Struct tGrokModel
+    String id
+    Integer created
+    {Name=object }
+    String _object
+    String owned_by
+End_Struct
+
+Struct tGrokModelList
+    tClaudeModel[] data
+    {Name=object }
+    String _object
+End_Struct
